eec2277c57cf8788bbadfdf8e5d7d00c3b2cb550
Steven Woods <steven.w89@hotmail.co.uk>
2019-01-22 23:02:44 +0000

    Neater processing of input

--- Day 1 - Chronal Calibration/chronal_calibration.py
+++ Day 1 - Chronal Calibration/chronal_calibration.py
@@ -6,24 +6,19 @@
 import sys
 inFile = sys.argv[1]
 
-frequency_shifts = []
-f = open(inFile, "r")
-for change in f:
-	change = int(change.rstrip())
-	frequency_shifts.append(change)
-f.close()
-# frequency_shifts is a list containing all the changes in frequency as integers
+with open(inFile, "r") as f:
+	frequency_shifts = [int(change.rstrip()) for change in f]  # List of frequency changes
 
-(current_frequency, first_round, calibrated) = (0, (),())  
+current_frequency, first_round, calibrated = 0, (), ()
 counts = dict()
-while not calibrated:    # Until the same number (frequency) is seen twice, the next change in the list is applied
+while not calibrated:  # Next change applied until same number (frequency) is seen twice.
 	for change in frequency_shifts:
 		counts[current_frequency] = counts.get(current_frequency, 0) + 1
-		if counts[current_frequency] is 2:	# The same number (frequency) has been seen twice - device is calibrated
+		if counts[current_frequency] is 2:	# The same number (frequency) seen twice: "device calibrated"
 			calibrated = current_frequency  # The frequency at calibration is stored for later output
 			break
 		current_frequency += change
-	if not first_round:						# Frequency after one iteration through list of changes is stored for output.
+	if not first_round:						# Frequency after one iteration through changes stored for output.
 		first_round = current_frequency
 
 print (f"The resulting frequency after all the changes in frequency have been applied is {first_round}.")
