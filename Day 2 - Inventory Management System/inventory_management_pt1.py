116f5f508ae29259020e37602f805898cd1cfafc
Steven Woods <steven.w89@hotmail.co.uk>
2019-01-23 16:59:00 +0000

    "Updated with shorter code"

--- Day 2 - Inventory Management System/inventory_management_pt1.py
+++ Day 2 - Inventory Management System/inventory_management_pt1.py
@@ -6,25 +6,19 @@
 import sys
 inFile = sys.argv[1]
 
-(box_count2, box_count3) = (0, 0)
-IDs = open(inFile, "r")
-for ID in IDs:
-	counts = dict()
-	ID = ID.rstrip()
-	while ID:
-		letter = ID[0]  # Look at first letter in string
-		counts[letter] = counts.get(letter, 0) + 1  # Count letter
-		ID = ID[1:]   # Remove the counted letter from string
-	(letter_count2, letter_count3) = (False, False)
-	for letter in counts:
-		if counts[letter] is 2:
-			letter_count2 = True
-		if counts[letter] is 3:
-			letter_count3 = True
-	if letter_count2 is True:
-		box_count2 += 1
-	if letter_count3 is True:
-		box_count3 += 1
-IDs.close()
+box_count2, box_count3 = 0, 0
+with open(inFile, "r") as boxes:
+	for box in boxes:
+		counts = dict()
+		letter_count2, letter_count3 = False, False
+		for letter in box:
+			if box.count(letter) is 2:
+				letter_count2 = True
+			if box.count(letter) is 3:
+				letter_count3 = True
+		if letter_count2 is True:
+			box_count2 += 1
+		if letter_count3 is True:
+			box_count3 += 1
 
 print (f"{box_count2} boxes have two of the same letter and {box_count3} boxes have three of the same letter. Checksum = {box_count2} * {box_count3} = {box_count2 * box_count3}")
--- Day 2 - Inventory Management System/inventory_management_pt2.py
+++ Day 2 - Inventory Management System/inventory_management_pt2.py
@@ -3,39 +3,22 @@
 # Day 2: Inventory Management System, part 2
 # To run: python3 inventory_management_pt2.py <path/to/input_file>
 
+
+def match_test(boxes):
+	for box in boxes:
+		for other_box in boxes:
+			if box != other_box:
+				for i in range(len(box)):
+					if box[:i] + box[i+1:] == other_box[:i] + other_box[i+1:]:
+						print(f"Boxes {box} and {other_box} are matching boxes.")
+						return (box[:i] + box[i+1:])
+
+
 import sys
 inFile = sys.argv[1]
 
-boxes = []
-b = open(inFile, "r")
-for box in b:
-	box = box.rstrip()
-	boxes.append(box)
-b.close()
+with open(inFile, "r") as b:
+	boxes = [box.rstrip() for box in b]
 
-matching_pair = []
-for box in boxes:
-	if not matching_pair:
-		matches = dict()
-		i = 0
-		while i < len(box):
-			matches[i] = []
-			for other_box in boxes:   # NB each box also compared to self
-				if other_box[i] == box[i]:
-					matches[i].append(other_box)	# Store IDs with common letter at that position
-			i += 1
-		counts = dict()
-		for letter_pos in matches:
-			for other_box in matches[letter_pos]:
-				counts[other_box] = counts.get(other_box, 0) + 1
-		for other_box in counts:
-			if counts[other_box] is len(box) - 1:
-				print (f"Boxes {box} and {other_box} are the correct boxes.")
-				matching_pair = (box, other_box)
-
-i = 0
-while matching_pair[0][i] == matching_pair[1][i]:
-	i += 1
-common_letters = matching_pair[0][0:i] + matching_pair[0][i+1:]   # Removes the mismatch
-
+common_letters = match_test(boxes)			
 print (f"The common letters are {common_letters}.")
